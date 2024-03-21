9909a6d8fc2ee05568089a8c76918cf1c8257436
SAMUEL TAI ZI WEI <b022210152@student.utem.edu.my>
2024-03-21 21:44:49 +0800

    new update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 
 app.get('/', (req, res) => {
-   res.send('Testing for class!')
+   res.send('Te sting for class!')
 })
 
 app.listen(port, () => {
