34af06cfd4344f939054801a1e1c8217e3fa0916
stzw1109 <163002436+stzw1109@users.noreply.github.com>
2024-03-21 14:23:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send('Testing!')
+})
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
+})
