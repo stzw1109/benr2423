746de0a754814ccfb121dd09c17544486ce4d32e
SAMUEL TAI ZI WEI <b022210152@student.utem.edu.my>
2024-03-28 21:58:27 +0800

    update 28/3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(port, () => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://samuel:St020911080741$@benr2423.jgm92s9.mongodb.net/?retryWrites=true&w=majority&appName=BENR2423";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,12 +29,60 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    //insert one document with the said information
+    /*let result = await client.db("testing").collection("file_1").insertOne({
+        name: "Samuel",
+        age: 20,
+        gender:"male",
+        faculty: "FTKEK"
+      });
+    */
+
+   //to find the document with the said information
+   
+    let result = await client.db("testing").collection("file_1").find({name:'Samuel'},
+    {
+      //projection is used to select the fields to be displayed
+      projection:{age:0,gender:0}
+      }
+        ).toArray();
+    
+   //update a document with the said information
+   /*
+    let result = await client.db("testing").collection("file_1").updateOne(
+      {_id:new ObjectId('660518d9aa2fa53121de3f66')},
+      {
+        $set:{
+          faculty:'FTKEK',
+          gender:'female'
+        },
+        //push must be used for array only
+        $push:{
+          faculty:'FKEKK',
+          gender: 'male'
+        }
+        
+      }
+    );
+    */
+   //delete a document with the said information
+   /*
+    let result = await client.db("testing").collection("file_1").deleteOne(
+      {
+        _id:new ObjectId('65efc07a441e4b25a99534c0')
+      }
+    );
+   */ 
+   console.log(result);
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
