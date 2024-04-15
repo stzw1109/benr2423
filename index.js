d1d7ecf959e973d2cfe0a023bd965f7b6f362c5b
SAMUEL TAI ZI WEI <b022210152@student.utem.edu.my>
2024-04-15 17:14:03 +0800

    new changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
@@ -7,14 +8,38 @@ app.use(express.json())
 //e.g using for registration
 app.post('/register',async(req,res) => {
   //console.log(req.body);
+
+  const hash = bcrypt.hashSync(req.body.password, 10);
+
   let resq = await client.db("testing").collection("file_1").insertOne({
-        name: req.body.name,
+        name: req.body.username,
         age: req.body.age,
         gender: req.body.gender,
-        faculty: req.body.faculty
+        faculty: req.body.faculty,
+        password: hash //req.body.password
     });
     res.send(resq);
 })
+
+app.post('/login',async(req,res) => { 
+      let resp = await client.db("testing").collection("file_1").findOne({
+        username: req.body.username
+})
+  console.log(resp);
+  console.log(req.body);
+
+      if(!resp){
+        res.send('User not found');
+      }else{
+        if(bcrypt.compareSync(req.body.password,resp.password)==true){
+          res.send('Login successful');
+        } else{
+          res.send('Wrong Password');
+        
+        }
+      }
+      
+});
 
 //get read user profile
 app.get('/read/:username/:email',async(req,res) => {
